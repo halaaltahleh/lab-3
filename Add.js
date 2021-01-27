97285585d1716adc698c0904beef4ca9da5c19db
Hala ALtahleh <halaaltahleh80@gmail.com>
2021-01-27 17:06:43 +0200

    commit

--- Add.js
+++ Add.js
@@ -1,19 +1,21 @@
-var NewspaperType = prompt("what type of newspaper do you want?")
-var noNewspaper = prompt("what mean of newspaper do you want in a one week?")
+var NewspaperType = prompt("what type of newspaper do you want?"); //sport 
+var noNewspaper = prompt("what mean of newspaper do you want in a one week?"); //22
 
 var OrderDetails;
+
 if (noNewspaper >= 1 && noNewspaper < 5) {
-    OrderDetails = " congracts You can get two newspapers for free"
+    OrderDetails = " congracts You can get two newspapers for free";
 }
 else if (noNewspaper > 10) {
-    OrderDetails = "Congratulations you got free delivery of your newspaper every day"
+    OrderDetails = "Congratulations you got free delivery of your newspaper every day";
 }
 else {
-    OrderDetails = "We apologize. There is an error"
+    OrderDetails = "We apologize. There is an error";
 }
 
 
+document.write(OrderDetails);
+confirm("welcom");
+alert("thank you");
 
-
-document.write(OrderDetails)
 
