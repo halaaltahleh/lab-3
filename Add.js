89f65efce2bd2bcc6a07f424e44450ef1ae473b0
Hala ALtahleh <halaaltahleh80@gmail.com>
2021-01-27 16:30:33 +0200

    commit

--- Add.js
+++ Add.js
@@ -1,1 +1,11 @@
 var NewspaperType = prompt("what type of newspaper do you want?")
+var noNewspaper = prompt("what mean of newspaper do you want in a one week?")
+
+var OrderDetails;
+if (noNewspaper >= 1 && noNewspaper < 5) {
+    OrderDetails = " congracts You can get two newspapers for free"
+}
+else if (noNewspaper > 10) {
+    OrderDetails = "Congratulations you got free delivery of your newspaper every day"
+}
+
